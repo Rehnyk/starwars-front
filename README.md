672674835b16568039d8f01b131aa4c41e5d82db
Rehnyk <hnykova.renata@gmail.com>
2025-07-14 16:48:50 +0200

    Phase 4 - Add new comments, live demo

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 
 # Starwars Front-End project
 
-
-Pro spuštění projektu ve vývojovém režimu postupujte následovně:
+## Lokální spuštění
+Aplikaci lze spustit lokálně pomocí Vite:
 
 ```bash
 npm install
 npm run dev
 ```
+Výchozí port Vite je 5173, aplikace je tedy dostupná na : ```http://localhost:5173```
 
-### Poznámka k build procesu
-Projekt v aktuální fázi neobsahuje produkční build proces, proto je doporučeno používat pouze vývojový server (npm run dev) pro lokální testování.
+## Produkční build
+Produkční build lze vytvořit příkazem: ```npm run build```
+
+## Živé demo
+Projekt lze najit na adrese https://starwars-demo-renata.vercel.app
 
 ## Fáze 1 - Statické stránky
 V prvotní fázi projektu jsem vytvořila statické webové stránky podle poskytnutého grafického návrhu. Zaměřila jsem se na co nejpřesnější vizuální zpracování a zajištění responzivity napříč různými zařízeními.
@@ -21,8 +25,15 @@ V dalším kroku jsem implementovala dynamické načítání seznamu filmů pomo
 
 Doporučené API bohužel není funkční, ale našla jsem alternativu na `https://swapi.py4e.com`
 
-## 3. fáze – Interaktivní mapa
+## Fáze 3 – Mapa s markery
 Do projektu jsem integrovala knihovnu **Leaflet.js**. Mapa zobrazuje marker s popupem a červeným kruhem dle zadání.
+
+## Fáze 4 – Komentáře v localStorage a validací
+
+V poslední fázi jsem umožnila přidávání komentářů,  které jsou ukládány do localStorage. Zahrnuta je také validace na minimálně 3 znaky.
+
+Součástí *Comment section* je také toggle tlačítko pro zobrazení více či méně komentářů (implementováno již ve fázi 1).
+
 
 ## Použité technologie
 * **Svelte** - framework pro strukturu a logiku aplikace
--- src/components/CommentSection.svelte
+++ src/components/CommentSection.svelte
@@ -1,24 +1,54 @@
 <script>
+  import { onMount } from "svelte";
   import SectionLabel from "./SectionLabel.svelte";
 
   export let label = {};
   export let commentsData = [];
 
+  let userComments = [];
+  let commentText = "";
+  let error = "";
+
   let showAll = false;
   const visibleCount = 2;
+
+  $: comments = [...userComments, ...commentsData].sort(
+    // @ts-ignore
+    (a, b) => new Date(b.time) - new Date(a.time),
+  );
+
+  $: visibleComments = showAll ? comments : comments.slice(0, visibleCount);
+
+  onMount(() => {
+    const stored = localStorage.getItem("userComments");
+    userComments = stored ? JSON.parse(stored) : [];
+  });
+
+  function addComment() {
+    error = "";
+    if (commentText.trim().length < 3) {
+      error = "At least 3 characters required";
+      return;
+    }
+
+    const newComment = {
+      time: new Date().toISOString(),
+      text: commentText.trim(),
+    };
+
+    userComments = [newComment, ...userComments];
+    localStorage.setItem("userComments", JSON.stringify(userComments));
+    commentText = "";
+  }
 
   function formatDate(isoString) {
     if (!isoString) return "";
     const date = new Date(isoString);
-    const day = String(date.getDate());
-    const month = String(date.getMonth() + 1);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
     return `${day}.${month}.${year}`;
   }
-
-  $: visibleComments = showAll
-    ? commentsData
-    : commentsData.slice(0, visibleCount);
 </script>
 
 <section id="comments" class="comments-section position-relative w-75 mx-auto">
@@ -27,19 +57,29 @@
   <div class="container">
     <div class="card p-5 comment-input-card shadow rounded-0 border-0">
       <div class="d-flex align-items-center">
+        {#if error}
+          <small class="text-danger error-text position-absolute"
+            >At least 3 characters required</small>
+        {/if}
         <textarea
           name="write-comment"
           class="comment-textarea form-control border-0 border-bottom rounded-0 shadow-none mt-4"
+          bind:value={commentText}
           placeholder="Write your comment here ..."
+          on:input={() => (error = "")}
         ></textarea>
-        <button class="btn comment-send-btn text-muted" aria-label="send comment">
+        <button
+          class="btn comment-send-btn text-muted"
+          on:click={addComment}
+          aria-label="send comment"
+        >
           <i class="fa-solid fa-circle-chevron-right"></i>
         </button>
       </div>
     </div>
 
     <h3 class="text-uppercase fs-5 text-muted py-4 m-4">
-      Comments: {commentsData.length}
+      Comments: {comments.length}
     </h3>
 
     <div class="comments-list">
@@ -53,7 +93,7 @@
       {/each}
     </div>
 
-    {#if commentsData.length > visibleCount}
+    {#if comments.length > visibleCount}
       <div class="text-center">
         <button
           class="btn btn-outline-secondary rounded-0 shadow-sm"
@@ -79,8 +119,14 @@
     margin-left: -3rem;
     font-size: 3rem;
   }
+
   .comment-send-btn:hover {
     color: #9e9e9e !important;
   }
 
+  .error-text {
+    top: 2.5rem;
+    left: 3.5rem;
+    font-size: 0.8rem;
+  }
 </style>
